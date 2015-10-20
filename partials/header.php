6568962e3a58eea5b31a6eeb73c3665b69eb5170
Sebastian Sulinski <Sebastian@Sebastians-Mac-Pro.local>
2015-10-20 14:29:40 +0100

    Moved lang attribute to the html

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-<html>
-<head lang="en">
+<html lang="en">
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <title><?php echo $title; ?></title>
